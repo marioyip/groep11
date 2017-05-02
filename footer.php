4ebd2f70122985fd7266b4295f43c6185dbca5a2
estherdh <ED.Hofwijks@student.han.nl>
2017-05-02 14:42:40 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,4 +1,32 @@
 </main>
-<footer>&copy; <?=date("Y")?> - EenmaalAndermaal</footer>
+<footer>
+    <nav class="navbar navbar-default navbar-fixed-bottom ">
+        <div class="container-fluid">
+    <ul class="nav navbar-nav">
+        <li class="affix">&copy; <?=date("Y")?> - EenmaalAndermaal</li>
+        <div class="btn-link">
+            <li>
+                <a href="infopagina.html">Help &amp; info</a>
+            </li>
+            <li>
+                <a href="algemenevoorwaarden.html">Algemene voorwaarden</a>
+            </li>
+            <li>
+                <a href="privacyverklaring.html">Privacy</a>
+            </li>
+            <li>
+                <a href="contact.html">Contact</a>
+            </li>
+            <li>
+                <a href="overons.html">Over ons</a>
+            </li>
+            <li>
+                <a href="solliciteren.html">Werken bij</a>
+            </li>
+    </ul>
+        </div>
+        </div>
+    </nav>
+</footer>
 </body>
 </html>
