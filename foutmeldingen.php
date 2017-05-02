d5507c0ad9b2c4d1db2f7badd41c4379d57c683a
estherdh <ED.Hofwijks@student.han.nl>
2017-05-02 13:48:48 +0200

    ddd

---
+++ foutmeldingen.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Esther
+ * Date: 5/2/2017
+ * Time: 1:47 PM
+ */
--- inloggen.php
+++ inloggen.php
@@ -3,13 +3,13 @@ include('header.html');
 //pdo_connect();
 //login();
 ?>
-<!--    <form id="aanmelden" method="post" action="foutmeldingen.php">-->
-<!--        <label for="gebruikersnaam">Gebruikersnaam</label><input type="text" name="gebruikersnaam"-->
-<!--                                                     placeholder="Gebruikersnaam" value="-->
-<!--        <label for="achternaam">Achternaam</label><input type="text" name="achternaam" id="achternaam"-->
-<!--                                                         placeholder="Achternaam" value="-->
-<!--        <input id="regaanmelden" type="submit" name="aanmelden" value="Aanmelden"/>-->
-<!--    </form>-->
+    <form id="inloggen" method="post" action="foutmeldingen.php">
+        <label for="gebruikersnaam">Gebruikersnaam</label><input type="text" name="gebruikersnaam"
+                                                     placeholder="Gebruikersnaam">
+        <label for="achternaam">Achternaam</label><input type="text" name="achternaam" id="achternaam"
+                                                         placeholder="Achternaam">
+        <input id="regaanmelden" type="submit" name="aanmelden" value="Aanmelden"/>
+    </form>
 <?php
 include('footer.php');
 ?>
