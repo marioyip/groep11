a9b812ef7e946d0d2ff1fe3c45acc7f4daf0eaf6
Mario Yip <marioyip@msn.com>
2017-05-03 16:08:53 +0200

    Form v1.0 registeren.php

--- registreren.php
+++ registreren.php
@@ -134,6 +134,7 @@ include 'header.html';
                         </div>
                     </div>
 
+
                     <div class="form-group">
                         <div class="col-sm-offset-2 col-sm-10">
                             <button type="submit" class="btn btn-default">Submit</button>
