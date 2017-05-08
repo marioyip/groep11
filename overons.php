4e5e06bf2c53b7351740a4c735666dd7876c8b25
estherdh <ED.Hofwijks@student.han.nl>
2017-05-08 09:10:45 +0200

    over ons, footer contact

--- overons.php
+++ overons.php
@@ -16,60 +16,6 @@
 <?php include 'header.html';
 ?>
 <main>
-<!-- Full Page Image Background Carousel Header -->
-<div id="myCarousel" class="carousel slide">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for Slides -->
-    <div class="carousel-inner">
-        <div class="item active">
-            <!-- Set the first background image using inline CSS below. -->
-            <div class="fill" style="background-image:url('media/grasmaaier.JPG');"></div>
-            <div class="carousel-caption">
-                <h2>Grasmaaier</h2>
-            </div>
-        </div>
-        <div class="item">
-            <!-- Set the second background image using inline CSS below. -->
-            <div class="fill" style="background-image:url('media/fauteuil.jpg');"></div>
-            <div class="carousel-caption">
-                <h2>Fauteuil</h2>
-            </div>
-        </div>
-        <div class="item">
-            <!-- Set the third background image using inline CSS below. -->
-            <div class="fill" style="background-image:url('media/laptop.png');"></div>
-            <div class="carousel-caption">
-                <h2>Laptop</h2>
-            </div>
-        </div>
-    </div>
-
-    <!-- Controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-        <span class="icon-prev"></span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-        <span class="icon-next"></span>
-    </a>
-
-<!-- jQuery -->
-<script src="js/jquery.js"></script>
-
-<!-- Bootstrap Core JavaScript -->
-<script src="js/bootstrap.min.js"></script>
-
-<!-- Script to Activate the Carousel -->
-<script>
-    $('.carousel').carousel({
-        interval: 5000 //changes the speed
-    })
-</script>
 
 </body>
 </html>
