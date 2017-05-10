90a1dcfb7154ce15090528604f02d5f5247d1553
hugoook <hugo4ever@live.nl>
2017-05-10 14:56:17 +0200

    zoekpagina de footer staat nu op de bottom

--- footer.php
+++ footer.php
@@ -54,7 +54,7 @@
                             <div class="input-append newsletter-box text-center">
 <!--                                <input title="inschrijven" type="text" class="full text-center textDarkGray form-control fontSize16"><br>-->
                                 <input class="full text-center textDarkGray form-control fontSize16" type="text" name="name" id="user_name" placeholder="Naam"/>
-                                <input class="full text-center textDarkGray form-control fontSize16" type="text" name="email" id="email" placeholder="E-mail"/>
+                                <input class="full text-center textDarkGray form-control fontSize16" type="email" name="email" id="email" placeholder="E-mail"/>
 <!--                                <button class="textDarkGray newsButtonGray bg-gray btn btn-default " type="submit">Inschrijven </button>-->
                                 <input class="textDarkGray newsButtonGray bg-gray btn btn-default" type="submit" value="Inschrijven" name="submit_form"/>
                             </div>
