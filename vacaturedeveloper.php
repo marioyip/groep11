85f092a84943d1879eaa49da89e4ce54d8d249cd
Mario Yip <marioyip@msn.com>
2017-05-13 20:06:14 +0200

    Vacature pagina's toegevoegd. Achtergrondafbeelding toegevoegd.

--- vacaturedeveloper.php
+++ vacaturedeveloper.php
@@ -38,21 +38,17 @@
                             <ul>
 
                                 <h3 class="textGreen">Over de functie</h3>
-                        <p>Als medewerker van Customer Care ben jij het eerste aanspreekpunt
-                            voor onze (potentiële) klanten. We doen er alles aan om het zo makkelijk mogelijk te
-                            maken voor klanten en willen onze service continu verbeteren. Je beantwoordt vragen via
-                            telefoon en e-mail en adviseert zowel klanten als verkopers over de service die we
-                            bieden
-                            en verzamelt feedback om de service te blijven verbeteren.
-                            Als medewerker van ons klantenservice team kan jij het verschil maken!</p>
+                        <p>Je bent als collega binnen het team Leren & Ontwikkelen onderdeel van de afdeling IT.
+                            Je wordt aangestuurd door de Manager Leren & Ontwikkelen en hebt de
+                            Studiefabriekbaas, Medewerker Leren & Ontwikkelen en Specialist Leren & Ontwikkelen als
+                            directe collega’s.</p>
 
                         <h3 class="textGreen marginTop50">De verantwoordelijkheden zijn onder andere:</h3>
                         <li>
-                            Beantwoorden van vragen via telefoon en e-mail.
+                            Je ontwerpt, implementeert, evalueert en optimaliseert trainingen en trainingspakketten voor collega's op specifieke afdelingen.
                         </li>
                         <li>
-                            Contact met zowel klanten als schoonmaakhulpen – jij bent het eerste aanspreekpunt voor
-                            beide partijen en komt samen met hen tot de beste oplossing.
+                            Je adviseert op afdelingsniveau bij specifieke leer- en ontwikkelvraagstukken.
                         </li>
                         <li>
                             Continu meedenken en meewerken aan het verbeteren van ons platform.
@@ -63,27 +59,26 @@
                             Je bent flexibel en je kan je goed aanpassen aan veranderingen.
                         </li>
                         <li>
-                            Je denkt in oplossingen en hebt een positieve instelling.
+                            Je bent bekend met verschillende werkvormen, leerstijlen en past deze kennis praktisch toe in het ontwerpproces en alle gesprekken.
                         </li>
                         <li>
-                            Je neemt graag initiatief.
+                            Je hebt minimaal vijf jaar ervaring als PHP Developer.
                         </li>
                         <li>
-                            Je hebt een goede beheersing van de Nederlandse taal in zowel woord al geschrift.
+                            Scrum/agile is jouw tweede natuur.
                         </li>
                         <li>
-                            Je beheerst ook de Engelse taal voldoende om een gesprek over de telefoon te voeren en
-                            emails te schrijven.
+                            Ook een dikke plus wanneer je kennis hebt van Clean (Onion) architectuur en PaaS.
                         </li>
                         <h3 class="textGreen marginTop50">Wat bieden wij?</h3>
                         <li>
                             Ontwikkelingsmogelijkheden binnen een groeiende start-up.
                         </li>
                         <li>
-                            Een gezellige werkomgeving met bijbehorende events.
+                            Ongekend goede werksfeer. Met versbereide happen, maandelijkse borrels, legendarische feesten, de leukste teamactiviteiten en een informele, speelse afdeling.
                         </li>
                         <li>
-                            Een markt-conforme vergoeding.
+                            Als je een idee hebt, staan we daar altijd voor open. Of je nu een week, een maand of een jaar bij ons werkt.
                         </li>
                         </ul>
                         </p>
--- vacatureprojectleider.php
+++ vacatureprojectleider.php
@@ -38,32 +38,34 @@
                             <ul>
 
                                 <h3 class="textGreen">Over de functie</h3>
-                        <p>Als medewerker van Customer Care ben jij het eerste aanspreekpunt
-                            voor onze (potentiële) klanten. We doen er alles aan om het zo makkelijk mogelijk te
-                            maken voor klanten en willen onze service continu verbeteren. Je beantwoordt vragen via
-                            telefoon en e-mail en adviseert zowel klanten als verkopers over de service die we
-                            bieden
-                            en verzamelt feedback om de service te blijven verbeteren.
-                            Als medewerker van ons klantenservice team kan jij het verschil maken!</p>
+                        <p>Je bent onderdeel van het team Faciliteiten, dat bestaat uit onze Facilitair Manager,
+                            Facilitair Teamleiders, Facilitair Projectleider, Facilitair Medewerkers en Gastvrouwen. Ook
+                            werk je elke dag samen met partners als schoonmaakbedrijven architecten en aannemers.
+                            Daarnaast schakel je met andere teams, waaronder Tech Services voor het realiseren van
+                            optimale werkplekken en team Merk voor de uitstraling van ons kantoor. Je collega’s van
+                            Procurement zijn je beste vrienden bij contractonderhandelingen. Je rapporteert aan
+                            Alessandro, de Facilitair Teamleider van EenmaalAndermaal!</p>
 
                         <h3 class="textGreen marginTop50">De verantwoordelijkheden zijn onder andere:</h3>
                         <li>
-                            Beantwoorden van vragen via telefoon en e-mail.
+                            Je zorgt voor het succesvol realiseren van meerdere projecten tegelijkertijd, aansluitend
+                            bij de wensen van jouw interne klant.
                         </li>
                         <li>
-                            Contact met zowel klanten als schoonmaakhulpen – jij bent het eerste aanspreekpunt voor
-                            beide partijen en komt samen met hen tot de beste oplossing.
+                            Je stuurt het projectteam aan en bewaakt de voortgang van het project.
                         </li>
                         <li>
-                            Continu meedenken en meewerken aan het verbeteren van ons platform.
+                            Je levert een bijdrage aan het bepalen van de doelstelling en de randvoorwaarden van
+                            verschillende projecten. Dit kan gaan om bijvoorbeeld planning, budget en beschikbare
+                            mensen.
                         </li>
 
                         <h3 class="textGreen marginTop50">Wie zoeken wij?</h3>
                         <li>
-                            Je bent flexibel en je kan je goed aanpassen aan veranderingen.
+                            Je bent verantwoordelijk voor het opstellen van planningen.
                         </li>
                         <li>
-                            Je denkt in oplossingen en hebt een positieve instelling.
+                            Je houdt je bezig met de onderhandelingen met aannemers.
                         </li>
                         <li>
                             Je neemt graag initiatief.
@@ -77,13 +79,13 @@
                         </li>
                         <h3 class="textGreen marginTop50">Wat bieden wij?</h3>
                         <li>
-                            Ontwikkelingsmogelijkheden binnen een groeiende start-up.
+                            Geld.
                         </li>
                         <li>
-                            Een gezellige werkomgeving met bijbehorende events.
+                            Een baan bij 1 van de snelst groeiende bedrijven van de Benelux.
                         </li>
                         <li>
-                            Een markt-conforme vergoeding.
+                            25 vakantiedagen. Als je belooft dat je terugkomt.
                         </li>
                         </ul>
                         </p>
