8fe56a7540adcb94779a931106bf33408e5deb2f
hugoook <hugo4ever@live.nl>
2017-05-15 10:30:52 +0200

    zoekpagina de footer staat nu op de bottom

--- zoekfunctie.php
+++ zoekfunctie.php
@@ -21,10 +21,8 @@
     include ('catbar.php');
     ini_set('display_errors', 1);
 
-    $pw = "rPgxSAaf";
-    $username = "iproject11";
-    $hostname = "mssql.iproject.icasites.nl";
-    $dbname = "iproject11";
+    connectToDatabase();
+    require_once('functies.php');
 
     $db = new PDO ("sqlsrv:Server=$hostname;Database=$dbname;ConnectionPooling=0", "$username", "$pw");//verbinding maken met de database
     if (isset($_GET["zoeken"]) && $_GET["zoeken"] != '') {
