79458c1546b84c96aaa8b9b380746a265817b549
estherdh <ED.Hofwijks@student.han.nl>
2017-05-15 10:44:59 +0200

    commentaar: footer, header, functies

--- functies.php
+++ functies.php
@@ -1,56 +1,41 @@
 <?php
 function connectToDatabase()
 {
-  $pw = "rPgxSAaf";
-  $username = "iproject11";
-   $hostname = "mssql.iproject.icasites.nl";
-   $dbname = "iproject11";
+    $pw = "rPgxSAaf";
+    $username = "iproject11";
+    $hostname = "mssql.iproject.icasites.nl";
+    $dbname = "iproject11";
 
     global $db;
 
     $db = new PDO ("sqlsrv:Server=$hostname;Database=$dbname;ConnectionPooling=0", "$username", "$pw");//verbinding maken met de database
-
 }
 
 function insertUserInDatabase($naam, $email)
 {
     global $db;
     try {
-        $stmt = $db->prepare("INSERT INTO Nieuwsbrief (name, email) VALUES (?,?)");
-        $stmt->execute(array($naam, $email));
-    } catch (PDOException $e) {
-        echo "Could not insert user, " . $e->getMessage();
+        $stmt = $db->prepare("INSERT INTO Nieuwsbrief (name, email) VALUES (?,?)"); //query aanmaken om de user in de databse te zetten
+        $stmt->execute(array($naam, $email)); //query word uitgevoerd om de naam en email in de database te zetten
+    } catch (PDOException $e) { //er wordt gekeken of er een uitzondering is waardoor de query niet uitgevoerd kan worden alleen dan wordt de volgende regel uitgevoerd
+        echo "Gebruiker kan niet in de database worden gezet, " . $e->getMessage();
     }
 }
-
-//function displayUsersInDatabase()
-//{
-//    global $pdo;
-//    try {
-//        $data = $pdo->query("SELECT * FROM ");
-//        while ($row = $data->fetch()) {
-//            echo "$row[Gebruikersnaam] $row[Wachtwoord]</br>";
-//        }
-//    } catch (PDOException $e) {
-//        echo "Could not read users, " . $e->getMessage();
-//    }
-//}
 
 function registerUser($voornaam, $achternaam, $email, $gebruikersnaam, $wachtwoord,
                       $geboortedatum, $vraag, $antwoord, $straat, $huisnr, $postcode,
                       $plaats, $land, $verkoper)
 {
-    echo "Hij komt hier!!!!!! <br>";
     global $db;
-    $hashedWachtwoord = password_hash($wachtwoord, 1);
+    $hashedWachtwoord = password_hash($wachtwoord, 1); //het meegegeven wachtwoord wordt gehashed
     try {
         $stmt = $db->prepare("INSERT INTO Gebruiker (Achternaam, Straatnaam1, Huisnummer1, 
 Antwoordtekst, GeboorteDag, Mailbox, Gebruikersnaam, Land, Plaatsnaam, Postcode, 
-Voornaam, Vraag, Wachtwoord, Verkoper) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+Voornaam, Vraag, Wachtwoord, Verkoper) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"); //query wordt aangemaakt om de user in de databse te kunnnen zetten
         $stmt->execute(array($achternaam, $straat, $huisnr, $antwoord, $geboortedatum,
             $email, $gebruikersnaam, $land, $plaats, $postcode, $voornaam,
-            $vraag, $hashedWachtwoord, $verkoper));
+            $vraag, $hashedWachtwoord, $verkoper)); //query word uitgevoerd om alle gegevens van de gebruiker in de database te zetten
     } catch (PDOException $e) {
-        echo "Could not insert user, " . $e->getMessage();
+        echo "Gebruiker kan niet in de database worden gezet, " . $e->getMessage();
     }
 }
