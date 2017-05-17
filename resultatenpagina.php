63143ea45738b65d79602758fc916850572079d7
Jroodhardt <julietteroodhardt@hotmail.com>
2017-05-17 14:31:09 +0200

    verbeterde resultaten1

--- resultatenpagina.php
+++ resultatenpagina.php
@@ -62,12 +62,14 @@ include 'catbar.php'; //geeft de cattegorieën balk mee aan deze pagina
             </div>
             <div class="col-md-10 container-fluid fixed">
                 <?php
+                $gekozenRubriek='Computers';
                 $lap=1;
-                for($i = 0; $i < 10; $i++) {
+                for($i = 1; $i < 6; $i++) {
                     echo '<div class="col-md-3 itemBox roundborder " align="center">
                 <img class="imgStyle roundborder" src="media/';
                     //                    Haalt de voorwerpcover, dus het plaatje uit de database en toont deze
-                    $sql = "SELECT voorwerpcover FROM Voorwerp WHERE Voorwerpnummer = $lap";
+                    $sql = "Select VoorwerpCover from voorwerp INNER JOIN voorwerpinrubriek ON voorwerp.voorwerpnummer = voorwerpinrubriek.voorwerp 
+                INNER JOIN rubriek on voorwerpinrubriek.RubriekOpLaagsteNiveau = rubriek.rubrieknummer WHERE Rubrieknaam = '$gekozenRubriek";
                     $stmt = $db->prepare($sql);
                     $stmt->execute();
 
@@ -76,7 +78,8 @@ include 'catbar.php'; //geeft de cattegorieën balk mee aan deze pagina
                     }
                     echo '<h4><a class="textDarkGray" href="productpagina.php">';
                     //Haalt de titel uit de database
-                    $sql = "SELECT Titel FROM Voorwerp WHERE Voorwerpnummer = $lap";
+                    $sql = "Select Titel from voorwerp INNER JOIN voorwerpinrubriek ON voorwerp.voorwerpnummer = voorwerpinrubriek.voorwerp 
+                INNER JOIN rubriek on voorwerpinrubriek.RubriekOpLaagsteNiveau = rubriek.rubrieknummer WHERE '$gekozenRubriek";
                     $stmt = $db->prepare($sql);
                     $stmt->execute();
 
@@ -86,7 +89,8 @@ include 'catbar.php'; //geeft de cattegorieën balk mee aan deze pagina
                     echo '</a></h4>';
                     echo '<div class="description">';
                     //Haalt de beschrijving uit de database
-                    $sql = "SELECT Beschrijving FROM Voorwerp WHERE Voorwerpnummer = $lap";
+                    $sql = "Select Beschrijving from voorwerp INNER JOIN voorwerpinrubriek ON voorwerp.voorwerpnummer = voorwerpinrubriek.voorwerp 
+                INNER JOIN rubriek on voorwerpinrubriek.RubriekOpLaagsteNiveau = rubriek.rubrieknummer WHERE Rubrieknaam = '$gekozenRubriek'";
                     $stmt = $db->prepare($sql);
                     $stmt->execute();
 
