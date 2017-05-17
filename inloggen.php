b9b7bb374e0377a31c05ac8d6b1457caccf7c6cb
estherdh <ED.Hofwijks@student.han.nl>
2017-05-17 13:14:04 +0200

    inloggen

--- inloggen.php
+++ inloggen.php
@@ -17,38 +17,50 @@ include 'header.php';
 require_once('functies.php');
 
 connectToDatabase();
-//global $db;
+global $db;
 
 //alle variabelen worden gedefinieerd en op lege waardes gezet
-$gebruikersnaam = $wachtwoord = "";
+$gebruikersnaam = $wachtwoord = $leegveld = "";
 $gebruikersnaamErr = $wachtwoordErr = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "hoi1";
-    if (empty($_POST["Gebruikersnaam"])) {
-        $gebruikersnaamErr = "De gebruikersnaam ontbreekt!";
+    if (empty($_POST["gebruikersnaam"]) || empty($_POST["wachtwoord"])) {
+        $leegveld = "De gebruikersnaam en het wachtwoord moeten beide ingevuld zijn!";
         echo "hoi2";
     } else {
-        $gebruikersnaam = test_input($_POST["Gebruikersnaam"]);
+        $gebruikersnaam = test_input($_POST["gebruikersnaam"]);
+        $wachtwoord = test_input($_POST['wachtwoord']);
         echo "hoi3";
     }
 
-    if (empty($_POST["Wachtwoord"])) {
-        $wachtwoordErr = "Voer uw wachtwoord in!";
-        echo "hoi4";
-    } else {
-        $wachtwoord = test_input($_POST["Wachtwoord"]);
-        echo "hoi5";
+    //Als password ok is dan true, anders false.
+    $password_ok = false;
+
+    $input['gebruikersnaam'] = $_POST['gebruikersnaam'] ?? '';
+    $ingevoerde_ww = $_POST['wachtwoord'] ?? '';
+
+    $stmt = $db->prepare('SELECT Wachtwoord FROM Gebruiker WHERE Gebruikersnaam = ?');
+    $stmt->execute([$input['gebruikersnaam']]);
+    $row = $stmt->fetch();
+
+    if ($row) {
+        $password_ok = password_verify($ingevoerde_ww,$row[15]);
     }
-}
+    if (!$password_ok) {
+        $errors[]='Voer een juiste gebruikersnaam en wachtwoord in!';
+    }
+    return array($input);
 
 //    $query1 = "SELECT * FROM Gebruiker WHERE Gebruikersnaam = $gebruikersnaam AND Wachtwoord = $wachtwoord";
 //    $stmt = $db->prepare($query1);
 //    $stmt->execute();
 //    if ($stmt->fetchColumn() == 1) {
+//        echo "jaaaaaa";
 //        session_start();
 //        $_SESSION['inloggen'];
 //    }
+}
 
 ?>
 <main>
@@ -62,28 +74,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="col-md-2 marginTop20 text-left">
             </div>
             <div class="col-md-4 marginTop20 text-left loginBox">
-                <form class="form-horizontal">
+                <form class="form-horizontal" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
                     <h3 class="">Inloggen</h3>
 
                     <div class="form-group marginBottom20">
                         <label class="control-label col-sm-3" for="email">Gebruikersnaam</label>
                         <div class="col-sm-10">
                             <input type="text" class="form-control" id="email" name="gebuikersnaam">
-                            <span
-                                class="error"><?php echo $gebruikersnaamErr; ?></span>
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="control-label col-sm-3" for="pwd">Wachtwoord</label>
                         <div class="col-sm-10">
                             <input type="password" class="form-control" id="pwd" name="wachtwoord">
-                            <span class="error"><?php echo  $wachtwoordErr; ?></span>
+                            <div class="alert alert-danger"><?= $leegveld ?></div>
                         </div>
                     </div>
                     <div class="form-group marginTop35">
                         <div class="col-sm-12">
                             <button id="regaanmelden" type="submit" name="inloggen" class="btn btn-default col-sm-4"
-                                    action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">Inloggen
+                                    action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"; method="post">Inloggen
                             </button>
                         </div>
                     </div>
