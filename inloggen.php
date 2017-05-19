de1a7eb428b69da7d2e222f309d61a8f1affd654
Jroodhardt <julietteroodhardt@hotmail.com>
2017-05-19 11:08:25 +0200

    Revert "inloggen gemaakt"
    This reverts commit ab193cf7021d9c7b63782bae9a6c1e555ca2ceab.
    
    # Conflicts:
    #	.idea/workspace.xml

--- inloggen.php
+++ inloggen.php
@@ -22,25 +22,38 @@ ini_set('display_errors', 'On');
 connectToDatabase();
 global $db;
 
-if(isset($_POST['submit'])){
-    $gebruikersnaam = $_POST['gebruikersnaam'];
-    $invgevoerdwachtwoord = $_POST['wachtwoord'];
+//alle variabelen worden gedefinieerd en op lege waardes gezet
+$gebruikersnaam = $wachtwoord = $leegveld = "";
+$gebruikersnaamErr = $wachtwoordErr = " ";
 
-    $sql = "SELECT Wachtwoord FROM Gebruiker WHERE Gebruikersnaam = '$gebruikersnaam'"; //De query maken
-    $stmt = $db->prepare($sql); //Statement object aanmaken
-    $stmt->execute();
-    while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-        $iswachtwoordgoed = password_verify($invgevoerdwachtwoord, $row[0]);
-        if ($iswachtwoordgoed == true) {
-            $_SESSION['username'] = $gebruikersnaam;
-            echo 'Welkom '.$_SESSION['username'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["gebruikersnaam"]) || empty($_POST["wachtwoord"])) {
+        $leegveld = "De gebruikersnaam en het wachtwoord moeten beide ingevuld zijn!";
+    } else {
+        $gebruikersnaam = test_input($_POST["gebruikersnaam"]);
+        $wachtwoord = test_input($_POST['wachtwoord']);
+        echo "hoi3";
+    }
+    if ($leegveld = "") {
+        //Als password ok is dan true, anders false.
+        $password_ok = false;
+
+        $input['gebruikersnaam'] = $_POST['gebruikersnaam'] ?? '';
+        $ingevoerde_ww = $_POST['wachtwoord'] ?? '';
+
+        $stmt = $db->prepare('SELECT Wachtwoord FROM Gebruiker WHERE Gebruikersnaam = ?');
+        $stmt->execute([$input['gebruikersnaam']]);
+        $row = $stmt->fetch();
+
+        if ($row) {
+            $password_ok = password_verify($ingevoerde_ww, $row[14]);
         }
-        else{
-            echo 'jammer vriend';
+        if (!$password_ok) {
+            $errors[] = 'Voer een juiste gebruikersnaam en wachtwoord in!';
         }
+        return array($input);
     }
 }
-
 
 ?>
 <main>
@@ -54,23 +67,31 @@ if(isset($_POST['submit'])){
             <div class="col-md-2 marginTop20 text-left">
             </div>
             <div class="col-md-4 marginTop20 text-left loginBox">
-                <form class="form-horizontal" method="post" action="#">
+                <form class="form-horizontal" method="post"
+                      action="">
                     <h3 class="">Inloggen</h3>
+
                     <div class="form-group marginBottom20">
-                        <label class="control-label col-sm-3" for="gebruikersnaam">Gebruikersnaam</label>
+                        <label class="control-label col-sm-3" for="email">Gebruikersnaam</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="gebruikersnaam" name="gebruikersnaam">
+                            <input type="text" class="form-control" id="email" name="gebuikersnaam">
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="control-label col-sm-3" for="pwd">Wachtwoord</label>
                         <div class="col-sm-10">
                             <input type="password" class="form-control" id="pwd" name="wachtwoord">
+                            <?php if ($leegveld = !"") {
+                                echo '<div class="alert alert-danger">' . $leegveld . '</div>';
+                            } ?>
                         </div>
                     </div>
                     <div class="form-group marginTop35">
                         <div class="col-sm-12">
-                            <input type="submit" name="submit" class="btn btn-default col-sm-4" value="SUBMIT">
+                            <button id="regaanmelden" type="submit" name="inloggen" class="btn btn-default col-sm-4"
+                                    action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" ; method="post">
+                                Inloggen
+                            </button>
                         </div>
                     </div>
                 </form>
