0ec53b81232aab9a879e30e2e586dad4d7a3ee9d
hugoook <hugo4ever@live.nl>
2017-05-31 09:53:41 +0200

    registercontrol emial

--- registercontrol.php
+++ registercontrol.php
@@ -32,7 +32,7 @@ echo $_SESSION['code'];
 
 //het schrijven van de email zelf
 $headers = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'From: EenmaalAndermaal Veiling <donotreply@eenmaalandermaal.nl>' . "\r\n";
+$headers .= 'From: EenmaalAndermaal Veiling <EenmaalAndermaal@iConcepts.nl>' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 $onderwerp = 'Bevestigingsmail EenmaalAndermaal' . "\r\n";
 $bericht = 'Dit is uw bevestigingscode: ' . $_SESSION['code'] . '' . "\r\n";
