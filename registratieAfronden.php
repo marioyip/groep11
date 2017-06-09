4917987748baf663f2fa281f70fdb7a224728c5d
DikkieDik123 <ds2110full@gmail.com>
2017-06-09 14:28:27 +0200

    I <3 HENK

--- registratieAfronden.php
+++ registratieAfronden.php
@@ -155,6 +155,7 @@
                     <div class="marginTop50">
                         <form class="form-horizontal" action="" method="POST">
                             <div class="form-group">
+                                <div class="col-sm-2"></div>
                                 <label class="control-label col-sm-2" for="gebruikersnaam">Gebruikersnaam:</label>
                                 <div class="col-sm-6">
                                     <input type="text" class="form-control" name="gebruikersnaam" id="gebruikersnaam"
@@ -162,6 +163,7 @@
                                 </div>
                             </div>
                             <div class="form-group">
+                                <div class="col-sm-2"></div>
                                 <label class="control-label col-sm-2" for="telefoonnummer">Telefoonnummer:</label>
                                 <div class="col-sm-6">
                                     <input type="text" class="form-control" id="telefoonnummer" name="telefoonnummer"
