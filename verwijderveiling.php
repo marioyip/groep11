125fced39e6ca8ca3db9c6abd4af1d4c53abdb20
DikkieDik123 <ds2110full@gmail.com>
2017-06-14 17:33:23 +0200

    Bugfixes hopelijk (moet getest worden)
    Als er feedback was gegeven of er was een bod gedaan was een veiling verwijderen onmogelijk

--- verwijderveiling.php
+++ verwijderveiling.php
@@ -1,19 +1,20 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Ruben
+ * User: Ruben & Dikkie
  * Date: 31-5-2017
  * Time: 15:01
  */
 session_start();
 require_once 'includes/functies.php';
 connectToDatabase();
-
 if (isset($_POST['voorwerp'])) {
     $voorwerp = $_POST['voorwerp'];
     $sql = "DELETE FROM Bestand WHERE voorwerp = '$voorwerp';
             DELETE FROM VoorwerpInRubriek WHERE Voorwerp = '$voorwerp';
-            DELETE FROM Voorwerp WHERE Voorwerpnummer = '$voorwerp'";
+            DELETE FROM Voorwerp WHERE Voorwerpnummer = '$voorwerp';
+            DELETE FROM Feedback WHERE Voorwerp = '$voorwerp';
+            DELETE FROM Bod WHERE Voorwerp = '$voorwerp';";
     $stmt = $db->prepare($sql);
     $stmt->execute();
 }
